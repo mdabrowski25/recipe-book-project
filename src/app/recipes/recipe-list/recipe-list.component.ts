369d76ed5e1f6e4ea12cc37b9d6cfeadb9ed4958
mdabrowski25 <m.dabrowski97@o2.pl>
2020-12-10 00:47:00 +0100

    Main fix, created example recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Simple test', 'https://memy.pl/show/big/uploads/Post/1456/14342964622259.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
