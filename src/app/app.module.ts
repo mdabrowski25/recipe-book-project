d05dedbff9b42dcedcf0b76688adfe4e60b092a0
mdabrowski25 <mdabrowski2511@gmail.com>
2021-03-29 15:50:23 +0200

    path not found update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.compo
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/modules/routing/routing.module.ts
+++ src/app/shared/modules/routing/routing.module.ts
@@ -3,19 +3,26 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {RecipesComponent} from '../../../recipes/recipes.component';
 import {ShoppingListComponent} from '../../../shopping-list/shopping-list.component';
+import {PathNotFoundComponent} from '../../../path-not-found/path-not-found.component';
 
 const routes: Routes = [{
-    path: '',
-    redirectTo: '/recipes',
-    pathMatch: 'full'
+  path: '',
+  redirectTo: '/recipes',
+  pathMatch: 'full'
 },
   {
-  component: RecipesComponent,
-  path: 'recipes'
-}, {
-  component: ShoppingListComponent,
-  path: 'shopping-list'
-}];
+    component: RecipesComponent,
+    path: 'recipes'
+  }, {
+    component: ShoppingListComponent,
+    path: 'shopping-list'
+  }, {
+    component: PathNotFoundComponent,
+    path: 'path-not-found'
+  }, {
+    path: '**',
+    redirectTo: 'path-not-found'
+  }];
 
 @NgModule({
   declarations: [],
