a665cb8ef2903366570a856986ba2c8ff6780584
mdabrowski25 <mdabrowski2511@gmail.com>
2021-03-31 20:00:30 +0200

    recipe-edit primary logic

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+    selector: 'app-recipe-edit',
+    templateUrl: './recipe-edit.component.html',
+    styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+    id: number;
+    editMode = false;
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.activatedRoute.paramMap.subscribe(params => {
+            this.id = +params.get('id');
+            this.editMode = params.get('id') != null;
+        });
+    }
 
 }
